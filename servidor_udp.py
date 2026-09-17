708e0de51869ed52b70955aafc0780027c0a2699
Michael Barros <michaelbarros.353@GMAIL.COM>
2026-09-16 21:38:57 -0300

    Add files via upload

---
+++ servidor_udp.py
@@ -0,0 +1,17 @@
+from xmlrpc.server import SimpleXMLRPCServer
+
+def soma(a, b):
+    return a + b
+
+def subtracao(a, b):
+    return a - b
+
+def multiplicacao(a, b):
+    return a * b
+
+servidor = SimpleXMLRPCServer(
+    ("localhost", 8000),
+    allow_none=True
+)
+
+servidor.register_function(soma, "soma")
